2cd4fb5703ddd7e22cbd1a89241026313e17e746
Ammarhere <52285757+Ammarhere@users.noreply.github.com>
2026-04-09 19:56:36 +0200

    Update TabNavigator.js

--- apps/customer/src/navigation/TabNavigator.js
+++ apps/customer/src/navigation/TabNavigator.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen    from '../screens/HomeScreen';
+import OrdersScreen  from '../screens/OrdersScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: '#E53935', tabBarLabelStyle: { fontWeight: '600' } }}>
+      <Tab.Screen name="Discover" component={HomeScreen} />
+      <Tab.Screen name="Orders"   component={OrdersScreen} />
+      <Tab.Screen name="Profile"  component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
