547d1c52bd42aeacb0f7f134ab4e1c91d63b623b
Ammarhere <52285757+Ammarhere@users.noreply.github.com>
2026-04-09 19:55:58 +0200

    Update authStore.js

--- apps/customer/src/store/authStore.js
+++ apps/customer/src/store/authStore.js
@@ -1,1 +1,12 @@
+import { create } from 'zustand';
 
+const useAuthStore = create(set => ({
+  user: null,
+  token: null,
+  isLoading: true,
+  setAuth:    (user, token) => set({ user, token, isLoading: false }),
+  logout:     ()            => set({ user: null, token: null, isLoading: false }),
+  setLoading: v             => set({ isLoading: v }),
+}));
+
+export default useAuthStore;
