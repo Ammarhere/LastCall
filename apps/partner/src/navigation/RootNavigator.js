aed7e4f49578a8f17677766f2740e17fc1af3daf
Ammarhere <52285757+Ammarhere@users.noreply.github.com>
2026-04-09 20:03:39 +0200

    Update RootNavigator.js

--- apps/partner/src/navigation/RootNavigator.js
+++ apps/partner/src/navigation/RootNavigator.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import useAuthStore from '../store/authStore';
+import LoginScreen  from '../screens/LoginScreen';
+import TabNavigator from './TabNavigator';
 
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  const token = useAuthStore(s => s.token);
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {token
+        ? <Stack.Screen name="Main"      component={TabNavigator} />
+        : <Stack.Screen name="Login"     component={LoginScreen} />}
+    </Stack.Navigator>
+  );
+}
