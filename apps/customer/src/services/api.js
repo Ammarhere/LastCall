1ab047f770afd39e93a608a965795486161da27a
Ammarhere <52285757+Ammarhere@users.noreply.github.com>
2026-04-09 19:55:41 +0200

    Update api.js

--- apps/customer/src/services/api.js
+++ apps/customer/src/services/api.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+import useAuthStore from '../store/authStore';
 
+const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:4000',
+});
+
+api.interceptors.request.use(cfg => {
+  const token = useAuthStore.getState().token;
+  if (token) cfg.headers.Authorization = `Bearer ${token}`;
+  return cfg;
+});
+
+api.interceptors.response.use(r => r, err => {
+  if (err.response?.status === 401) useAuthStore.getState().logout();
+  return Promise.reject(err);
+});
+
+export default api;
